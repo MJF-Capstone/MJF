d7bc4ea09004b7d2437ca57f3c5647448e0f490e
Frans Rivera Garcia <hopeprevails86@gmail.com>
2024-04-22 19:07:54 -0400

    login, update, delete

--- my-app/server/controllers/Stock.js
+++ my-app/server/controllers/Stock.js
@@ -4,62 +4,62 @@ const Stock = require('../models/stock.js');
 
 
 router.get('/stockdashboard', async (req, res) => {
-    try {
-      const stocks = await Stock.find({});
-      if (!stocks.length) {
-        return res.status(404).json({ message: 'No stock found.' })
+  try {
+    const stocks = await Stock.find({});
+    if (!stocks.length) {
+      return res.status(404).json({ message: 'No stock found.' })
     }
     res.status(200).json(stocks);
-    } catch (err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json({ message: `${err}` })
+  }
+});
+
+
+// router.get('/users', async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     if (!users.length) {
+//       return res.status(404).json({ message: 'No users found.' })
+//   }
+//   res.status(200).json(stocks);
+//   } catch (err) {
+//   console.log(err);
+//   res.status(500).json({ message: `${err}` })
+//   }
+// });
+
+router.get('/home', async (req, res) => {
+  res.json({ message: "Welcome to the Home page" })
+});
+
+
+router.post('/add', async (req, res) => {
+  try {
+    const { itemName, productNumber, stockCount, expirationDate, brandName } = req.body;
+    if (!itemName || !productNumber || !stockCount || !expirationDate || !brandName) {
+      return res.status(400).json({ message: 'All fields are required.' });
     }
-  });
-
-
-  // router.get('/users', async (req, res) => {
-  //   try {
-  //     const users = await User.find({});
-  //     if (!users.length) {
-  //       return res.status(404).json({ message: 'No users found.' })
-  //   }
-  //   res.status(200).json(stocks);
-  //   } catch (err) {
-  //   console.log(err);
-  //   res.status(500).json({ message: `${err}` })
-  //   }
-  // });
-
-  router.get('/home', async (req, res) => {
-    res.json({ message: "Welcome to the Home page"})
-  });
-  
-  
-  router.post('/add', async (req, res) => {
-    try {
-      const { itemName, productNumber, stockCount, expirationDate, brandName } = req.body;
-      if (!itemName || !productNumber || !stockCount || !expirationDate || !brandName) {
-        return res.status(400).json({ message: 'All fields are required.' });
-      }
-      const newStock = new Stock({
-        itemName,
-        productNumber,
-            stockCount,
-            expirationDate,
-            brandName
-          });
-          await newStock.save();
-        res.status(201).json(newStock);
-      } catch (error) {
-        console.error('Could not add new stock item:', error);
-      }
+    const newStock = new Stock({
+      itemName,
+      productNumber,
+      stockCount,
+      expirationDate,
+      brandName
     });
-    
+    await newStock.save();
+    res.status(201).json(newStock);
+  } catch (error) {
+    console.error('Could not add new stock item:', error);
+  }
+});
 
-    router.get('/', (req, res) => {
-        console.log("Something happened")
-        res.send('Hello World!!!');
-      });
-      
 
-      module.exports = router;
+router.get('/', (req, res) => {
+  console.log("Something happened")
+  res.send('Hello World!!!');
+});
+
+
+module.exports = router;
--- my-app/server/controllers/user-auth.js
+++ my-app/server/controllers/user-auth.js
@@ -12,11 +12,11 @@
 
 // module.exports = router
 
-const router = require('express').Router()
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const JWT_KEY = process.env.JWT_KEY
+const router = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const JWT_KEY = process.env.JWT_KEY;
 
 router.post('/register', async (req, res) => {
     try {
@@ -33,32 +33,42 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const { shopName, userId } = req.body;
-
-        const user = await User.findOne({ $or: [{ shopName }, { email: shopName }] })
+        const { shopName, employeeId } = req.body;
+        const user = await User.findOne({ shopName, employeeId });
         if (!user) {
-            return res.status(401).json({ error: "User Not found" });
+            return res.status(404).json({ error: "User not found" });
         }
-
-        const employeeIdMatch = await bcrypt.compare(userId, user.employeeId);
-        if (!employeeIdMatch) {
-            return res.status(401).json({ error: "Incorrect User Id" });
-        }
-
-        const token = jwt.sign({ userId: user._id }, JWT_KEY)
-
-        res.status(200).json({ message: "Login successful", token });
-
+        const token = jwt.sign({ userId: user._id }, JWT_KEY);
+        res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error("Error during login:", error);
-        res.status(500).json({ error: "Internal Server Error" })
+        res.status(500).json({ error: "Internal Server Error" });
     }
-})
+});
 
 
+router.put('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const { firstName, lastName, shopName, employeeId, isAdmin } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(userId, { firstName, lastName, shopName, employeeId, isAdmin }, { new: true });
+        res.status(200).json({ message: "User details updated", updatedUser });
+    } catch (error) {
+        console.error("Error updating user detail:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
+router.delete('/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
+module.exports = router;
 
-
-
-module.exports = router
