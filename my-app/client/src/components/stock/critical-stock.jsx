35be2614834148c47696db467ea31ff8c834d902
martha harrold <martharee392@gmail.com>
2024-04-23 21:56:47 -0400

    first update after file unlinked problem

--- my-app/client/src/components/stock/critical-stock.jsx
+++ my-app/client/src/components/stock/critical-stock.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 function CriticalStock() {
   return (
+    <div className="homeBackgroundImage" style={{
+      backgroundImage:
+"url('https://plus.unsplash.com/premium_photo-1675435644687-562e8042b9db?q=80&w=1049&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+    }}>
     <div className="App">
       <header className="App-header">
         <h1>Critical Stock Page</h1>
       </header>
+    </div>
     </div>
   );
 }
